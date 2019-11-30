715136e22b32dca26551470077757c83eb49aa54
amiradediran <amiradediran@gmail.com>
2019-11-30 14:56:57 +0100

    Create routes for components

--- site/src/App.js
+++ site/src/App.js
@@ -3,12 +3,18 @@ import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import Home from './components/home';
+import About from './components/about';
+import Work from './components/work';
+import Contact from './components/contact';
 
 
 function App() {
   return (
     <div className="App">
       <Route exact path = "/" component ={Home}/>
+      <Route exact path = "/about" component ={About}/>
+      <Route exact path = "/work" component ={Work}/>
+      <Route exact path = "/contact" component ={Contact}/>
     </div>
   );
 }
