2f9cf312304b833385873c0726c00ea7f5fa4219
amiradediran <amiradediran@gmail.com>
2019-11-30 15:02:14 +0100

    complete navigation component

--- site/src/components/nav.js
+++ site/src/components/nav.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-
-class Navigation extends React.Component{
-    render(){
-        return(
-            <div className = 'about-component'>
-                <h1>About </h1>
-            </div>
-        )
-    }
+function Navigation(){
+    return(
+        <div className = "nav-container">
+            <Link to = "/about">About</Link>
+            <Link to = "/work">Work</Link>
+            <Link to = "/contact">Contact</Link>
+        </div>
+    )
 }
+
 
 export default Navigation;
