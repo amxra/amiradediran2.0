440711b424c6a2a2a7a4aeffebcf50a7731130a1
amiradediran <amiradediran@gmail.com>
2019-12-02 13:51:16 +0100

    add work

--- site/src/components/work.js
+++ site/src/components/work.js
@@ -1,7 +1,12 @@
 import React from "react";
 import NavTwo from './nav2';
 import styled from 'styled-components';
-import site from './assets/amiradediran.png'
+import amiradediran2 from './assets/amiradediran2.png'
+import amiradediran from './assets/amiradediran.png'
+import restaurantpassport from './assets/restaurantpassport.png'
+import chefdiaries from './assets/chefdiaries.png'
+import howto from './assets/how-to.png'
+
 
 
 const StyledContainer = styled.div`
@@ -15,7 +20,7 @@ const StyledContainer = styled.div`
         height: 80%;
         margin: 0 auto;
         display: grid;
-        grid-template-columns: 25% 25% 25% 25%;
+        grid-template-columns: 30% 30% 30%;
         grid-template-rows: 30% 30% 30%;
         grid-column-gap: 10px;
         grid-row-gap: 10px;
@@ -24,8 +29,8 @@ const StyledContainer = styled.div`
             border: 1px solid black;
             padding: 1rem;
             img{
-                width: 98%;
-                height: 98%;
+                width: 70%;
+                height: 60%;
             }
         }
 
@@ -43,16 +48,29 @@ class Work extends React.Component{
                 <StyledContainer>
                  <div className = "work">
                      <div className = "box one">
-                         <a target = 'blank'href = "https://amxra.github.io/amiradediran/"><img src ={site} alt = 'site'/></a>
+                         <a target = 'blank'href = "https://amiradediran.com"><img src ={amiradediran2} alt = 'site'/></a>
+                         <p>This is the current version of my website built with React and jQuery</p>
                      </div>
-                     <div className = "box two">two</div>
-                     <div className = "box three">three</div>
-                     <div className = "box four">four</div>
-                     <div className = "box five">five</div>
-                     <div className = "box six">six</div>
-                     <div className = "box seven">seven</div>
-                     <div className = "box eight">eight</div>
-                     <div className = "box nine">nine</div>
+                     <div className = "box two">
+                        <a target = 'blank'href = "https://therestaurantpassport.netlify.com"><img src ={restaurantpassport} alt = 'site'/></a>
+                        <p>This is a multipage UI assigment designed and built by me using pure HTML and CSS</p>
+                     </div>
+                     <div className = "box three">
+                        <a target = 'blank'href = "https://front-end-dnn10lqgm.now.sh/login"><img src ={chefdiaries} alt = 'site'/></a>
+                        <p>This is a web application built with a team. I was a part of the frontend team that brought this to live! This was built using React</p>
+                     </div>
+                     <div className = "box four">
+                         <a target = 'blank'href = "https://how-to-dls5aefb1.now.sh/"><img src ={howto} alt = 'site'/></a>
+                         <p>This is a web application built with a team. I was a part of the frontend team that brought this to live! This was built using react</p>
+                     </div>
+                     <div className = "box five">
+                        <a target = 'blank'href = "https://amxra.github.io/amiradediran/"><img src ={amiradediran} alt = 'site'/></a>
+                         <p>This is the first version of my personal website. It was built using pure HTML and CSS.</p>
+                     </div>
+                     <div className = "box six">coming ...</div>
+                     <div className = "box seven">coming ...</div>
+                     <div className = "box eight">coming ...</div>
+                     <div className = "box nine">coming ...</div>
                  </div>
                  </StyledContainer>
             </div>
