8e8a8a21dc71d636ba8edba237ba4998b15cf288
amiradediran <amiradediran@gmail.com>
2019-12-03 13:18:53 +0100

    Update work

--- site/src/components/work.js
+++ site/src/components/work.js
@@ -18,7 +18,7 @@ const StyledContainer = styled.div`
 
     .work{
         width: 80vw;
-        height: 80%;
+        height: auto;
         margin: 0 auto;
         display: grid;
         grid-template-columns: 30% 30% 30%;
@@ -27,10 +27,14 @@ const StyledContainer = styled.div`
         grid-row-gap: 10px;
 
         .box{
+            box-sizing: border-box;
             border: 1px solid black;
             padding: 1rem;
-            
-            
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+       
 
             p{
                 font-size: 10px;
@@ -39,14 +43,26 @@ const StyledContainer = styled.div`
             ul li{
                 font-size: 8px;
                 display: inline;
-                border: 1px solid #FF6961;
+                border: 1px solid #686868;
                 margin: 0 0.5rem;
                 padding: 0 0.5rem;
             }
 
-            img{
-                width: 70%;
+            .box-image{
+                width: 80%;
                 height: 60%;
+
+                img{
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+
+            .box-stack{
+                
+                ul{
+                    margin:0;
+                }
             }
         }
 
@@ -64,57 +80,93 @@ class Work extends React.Component{
                 <StyledContainer>
                  <div className = "work">
                      <div className = "box one">
-                         <a target = 'blank'href = "https://amiradediran.com"><img src ={amiradediran2} alt = 'site'/></a>
-                         <p>This is the current version of my website</p>
-                         <ul className = "stack">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>jQuery</li>
-                            <li>React</li>
-                        </ul>
+                        <div className = 'box-image'>
+                            <a target = 'blank'href = "https://amiradediran.com"><img src ={amiradediran2} alt = 'site'/></a>
+                        </div>
+                        <div className = 'box-text'>
+                            <p>This is the current version of my website</p>
+                        </div>
+                        <div className = 'box-stack'>
+                            <ul className = "stack">
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>jQuery</li>
+                                <li>React</li>
+                            </ul>
+                        </div>
                      </div>
                      <div className = "box two">
-                        <a target = 'blank'href = "https://therestaurantpassport.netlify.com"><img src ={restaurantpassport} alt = 'site'/></a>
-                        <p>This is a multipage UI assigment designed </p>
-                        <ul className = "stack">
-                            <li>HTML</li>
-                            <li>CSS/LESS</li>
-                        </ul>
+                        <div className = 'box-image'>
+                            <a target = 'blank'href = "https://therestaurantpassport.netlify.com"><img src ={restaurantpassport} alt = 'site'/></a>
+                        </div>
+                        <div className = 'box-text'>
+                            <p>This is a multipage UI assigment designed </p>
+                        </div>
+                        <div className = 'box-stack'>
+                            <ul className = "stack">
+                                <li>HTML</li>
+                                <li>CSS/LESS</li>
+                            </ul>
+                        </div>
                      </div>
                      <div className = "box three">
-                        <a target = 'blank'href = "https://front-end-dnn10lqgm.now.sh/login"><img src ={chefdiaries} alt = 'site'/></a>
-                        <p>This is a web application built with a team. I was a part of the frontend team that brought this to live! I also deisgned this.</p>
-                        <ul className = "stack">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>React</li>
-                        </ul>
+                        <div className = 'box-image'>
+                            <a target = 'blank'href = "https://front-end-dnn10lqgm.now.sh/login"><img src ={chefdiaries} alt = 'site'/></a>
+                        </div>
+                        <div className = 'box-text'>
+                            <p>This is a web application built with a team. I was a part of the frontend team that brought this to live! I also deisgned this.</p>
+                        </div>
+                        <div className = 'box-stack'>
+                            <ul className = "stack">
+                                <li>HTML</li>
+                                <li>CSS/LESS</li>
+                                <li>React</li>
+                            </ul>
+                        </div>
                      </div>
                      <div className = "box four">
-                        <a target = 'blank'href = "https://sprint-rickandmorty.netlify.com/"><img src ={rickandmorty} alt = 'site'/></a>
-                         <p>This is a web application i designed and built using the rick and morty API</p>
-                         <ul className = "stack">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>React</li>
-                        </ul>
+                        <div className = 'box-image'>
+                            <a target = 'blank'href = "https://sprint-rickandmorty.netlify.com/"><img src ={rickandmorty} alt = 'site'/></a>
+                        </div>
+                         <div className = 'box-text'>
+                            <p>This is a web application i designed and built using the rick and morty API</p>
+                         </div>
+                         <div className = 'box-stack'>
+                            <ul className = "stack">
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>React</li>
+                            </ul>
+                        </div>
                      </div>
                      <div className = "box five">
-                         <a target = 'blank'href = "https://how-to-dls5aefb1.now.sh/"><img src ={howto} alt = 'site'/></a>
-                         <p>This is a web application built with a team. I was a part of the frontend team that brought this to live! I also designed this.</p>
-                         <ul className = "stack">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>React</li>
-                        </ul>
+                         <div className = 'box-image'>
+                            <a target = 'blank'href = "https://how-to-dls5aefb1.now.sh/"><img src ={howto} alt = 'site'/></a>
+                         </div>
+                         <div className = 'box-text'>
+                            <p>This is a web application built with a team. I was a part of the frontend team that brought this to live! I also designed this.</p>
+                         </div>
+                         <div className = 'box-stack'>
+                            <ul className = "stack">
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>React</li>
+                            </ul>
+                        </div>
                      </div>
                      <div className = "box six">
-                        <a target = 'blank'href = "https://amxra.github.io/amiradediran/"><img src ={amiradediran} alt = 'site'/></a>
-                         <p>This is the first version of my personal website.</p>
-                         <ul className = "stack">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                        </ul>
+                        <div className = 'box-image'>
+                            <a target = 'blank'href = "https://amxra.github.io/amiradediran/"><img src ={amiradediran} alt = 'site'/></a>
+                        </div>
+                         <div className = 'box-text'>
+                            <p>This is the first version of my personal website.</p>
+                         </div>
+                         <div className = 'box-stack'>
+                            <ul className = "stack">
+                                <li>HTML</li>
+                                <li>CSS</li>
+                            </ul>
+                        </div>
                      </div>
                      <div className = "box seven"></div>
                      <div className = "box eight"></div>
