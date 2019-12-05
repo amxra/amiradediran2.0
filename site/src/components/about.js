96d25f9f9e1c3eeea22991909cdb7b08acc2b9f0
amiradediran <amiradediran@gmail.com>
2019-12-05 22:31:25 +0100

    Complete resposivenss for about page

--- site/src/components/about.js
+++ site/src/components/about.js
@@ -4,130 +4,166 @@ import NavTwo from './nav2';
 import MobileNav from './mobilenav';
 
 
+const PageStyle = styled.div`
 
-const StyledContainer = styled.div`
-   border-sizing: box-border;
-   width: 100%;
-   height: 70vh;
-   display: flex;
-   align-items: center;
-   margin: 3rem 0;
-   justify-content: center;
+    box-sizing: border-box;
+    height: 100vh;
 
-   .aboutme{
-        border-sizing: box-border;
-        display:flex;
-        justify-content:space-between;
-        align-items: center;
-        margin-top: 3rem;
-        border: 1px solid black;
-        border-radius:2rem;
-        padding: 2rem;
-        height: 100%;
-        width: 80%;
-   }
-   
+    @media only screen and (max-width:800px){
+        // border: 1px solid yellow;
+        height: auto;
+    }
+    
 
-   .aboutme-image{
-       width: 40%;
-       height: 80%;
-       
-       img{
-           width: 80%;
-           height: 100%;
-       }
-   }
-   .aboutme-text{
-        box-sizing: border-box;
-        width: 50%;
-        height: 100%;
-        text-align: left;
+
+    .aboutme{
         display: flex;
-        flex-direction: column;
-        justify-content: center;
-        overflow: scroll;
+        justify-content: space-around;
+        flex-direction: row-reverse;
+        align-items: center;
+        height: 80vh;
+        
+        .aboutme-image{
+            width: 40%;
+            height: 70%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
 
-        ul{
-            list-style-type: none;
-            padding-left:0;
+
+            img{
+                width: 90%;
+                height: 100%;
+                         
+            }
         }
 
-        a{
-            text-decoration: none;
-            color:#FF6961;
-            padding-right: 1rem;
-        }
+        .aboutme-text{
+            width: 55%;
+            text-align: left;
+            padding: 1.5rem;
+            padding-left: 3rem;
 
-        a:hover{
-            color: #686868;
+            ul{
+                padding-left: 0;
+
+                li{
+                    list-style: none;
+
+                }
+            }
+
+            a{
+                text-decoration: none;
+                color:#FF6961;
+                margin-left: 1rem;
+
+            }
+
+            a:hover{
+                color: #686868;
+            }
         }
-   }
-}
+        
+    }
+
+    @media only screen and (max-width:800px){
+        .aboutme{
+            box-sizing: border-box;
+            width: 90%;
+            margin: 0 auto;
+            flex-direction: column;
+            height: auto;
+            margin-top: 10rem;
+        
+            .aboutme-image{
+                box-sizing: border-box;
+                width:80%;
+                height:500px;
+                
+            
+                    img{
+                        width: 100%;
+                        height: 100%;
+                    }
+            }
+        
+            .aboutme-text{
+                // box-sizing: border-box;
+                width: 100%;
+
+                
+            }
+        }
+    }
+
 `
+
 const HideNav = styled.div`
-
-@media only screen and (max-width: 768px){
-    .largenav{
-        display: none;
+    @media only screen and (max-width:800px){
+        .largenav{
+            display: none;
+        }
     }
-}
 
-@media only screen and (min-width: 767px){
-    .smallnav{
-        display: none;
+    @media only screen and (min-width:799px){
+        .smallnav{
+            display: none;
+        }
     }
-}
-
 `
 
-class About extends React.Component{
+class About extends React.Component {
     render(){
         return(
-            
-            <div className = "about-container">
-                <HideNav>
-                    <div className = 'largenav'>
-                        <NavTwo/>
-                    </div>
-                    <div className = 'smallnav'>
-                        <MobileNav/>
-                    </div>
-                </HideNav>
-                
-                <StyledContainer>
-                <div className = "aboutme">
-                    <div className = "aboutme-image">
+            <PageStyle>
+            <div className = 'about-container'>
+                <header>
+                    <HideNav>
+                        <div className = 'largenav'>
+                            <NavTwo/>
+                        </div>
+                        <div className = 'smallnav'>
+                            <MobileNav/>
+                        </div>
+                    </HideNav>
+                </header>
+                <div className = 'aboutme'>
+                    <div className = 'aboutme-image'>
                         <img src = "https://amiradediran.netlify.com/resources/media/7a8ac0be-87df-4375-9ba6-3361f3f0cd7b%202.JPG" alt = "author"/>
                     </div>
-                    <div className = "aboutme-text">
-                        <p>Hi, My name is Amira Adediran. I am a full stack developer. </p>
-                        <p>I build responsive, interactive, alluring websites and web applications using: 
-                            <ul>
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>JavaScript</li>
-                                <li>JQuery</li>
-                                <li>React</li>  
-                            </ul>
-                            I am also able to perform client-side authentication and implement CRUD operation.
-                        </p>
-                        <p>On the Backend, I use: 
-                            <ul>
-                                <li>Node.js</li>
-                                <li>Express</li>
-                            </ul>
-                            to build RESTful APIs. 
-                        </p>
-                        <p>I am passionate about all aspects of web technology.</p><p>When I'm not coding or tweeting you'll find me baking, sleeping or trying hard to motivate myself to work out, I also enjoy practicing yoga. </p>
-                        <p>Profiles: <a href = "https://www.github.com/amxra">Github</a> <a href = "https://codepen.io/amxra">Codepen</a> <a href = "https://www.linkedin.com/in/amira-adediran-8308b4196/">Linkedin</a></p>
+
+                    <div className = 'aboutme-text'>
+                    <p>Hi, My name is Amira Adediran. I am a full stack developer. </p>
+                         <p>I build responsive, interactive, alluring websites and web applications using: 
+                             <ul>
+                                 <li>HTML</li>
+                                 <li>CSS</li>
+                                 <li>JavaScript</li>
+                                 <li>JQuery</li>
+                                 <li>React</li>  
+                             </ul>
+                             I am also able to perform client-side authentication and implement CRUD operation.
+                         </p>
+                         <p>On the Backend, I use: 
+                             <ul>
+                                 <li>Node.js</li>
+                                 <li>Express</li>
+                             </ul>
+                             to build RESTful APIs. 
+                         </p>
+                         <p>I am passionate about all aspects of web technology.</p><p>When I'm not coding or tweeting you'll find me baking, sleeping or trying hard to motivate myself to work out, I also enjoy practicing yoga. </p>
+                         <p>Profiles: <a href = "https://www.github.com/amxra">Github</a> <a href = "https://codepen.io/amxra">Codepen</a> <a href = "https://www.linkedin.com/in/amira-adediran-8308b4196/">Linkedin</a></p>
+                   
                     </div>
+
                 </div>
-                </StyledContainer>
             </div>
-            
-            
+            </PageStyle>
         )
     }
 }
+
+
 
 export default About;
