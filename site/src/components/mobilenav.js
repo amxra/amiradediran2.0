f63f511b264f96e4ed9b7b2ace1996c1a448b59a
amiradediran <amiradediran@gmail.com>
2019-12-06 11:02:06 +0100

    fix mobile nav

--- site/src/components/mobilenav.js
+++ site/src/components/mobilenav.js
@@ -175,7 +175,7 @@ class MobileNav extends React.Component {
           position: 'absolute',
           top: 0,
           left: 0,
-          height: this.state.open? '100%': 0,
+          height: this.state.open? '100vh': 0,
           width: '100vw',
           display: 'flex',
           flexDirection: 'column',
